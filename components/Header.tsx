a7a9758c71ea3e20c9279bcf128e9f960d8eeaa2
Mario Krstevski <mariokrstevski@hotmail.com>
2024-01-08 13:43:29 +0100

    new project + navbar and footer

---
+++ components/Header.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import Container from "./ui/Container";
+import Navigation from "./Navigation";
+import getCategories from "@/actions/get-cateogries";
+import NavActions from "./NavActions";
+
+// so that is not cached and when we add a category on normal refresh it will show up, otherwise we needed to hard refresh
+export const revalidate = 0;
+
+export default async function Header() {
+  const categories = await getCategories();
+
+  return (
+    <div className="border-b">
+      <Container>
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+          <Link href={"/"} className="ml-4 flex lg:ml-0 gap-x-2">
+            <p className="font-bold text-xl">Store</p>
+          </Link>
+          <Navigation data={categories} />
+          <NavActions />
+        </div>
+      </Container>
+    </div>
+  );
+}
